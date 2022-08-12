b98375989eb3601e566322be7ae7ac239d028f98
Muhammad Shayan <mshayan@pie-int.com>
2022-08-12 11:15:33 +0500

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Muhammad Shayan on 12/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
